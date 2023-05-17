82f071af964f359ee883dba791b29d27bc0394ce
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-17 03:59:49 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+function App() {
+	// eslint-disable-next-line react/react-in-jsx-scope
+	return <h1 className="text-red-500">Hello world</h1>;
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: import.meta.API_KEY,
+	authDomain: import.meta.AUTH_DOMAIN,
+	projectId: import.meta.PROJECTID,
+	storageBucket: import.meta.STORAGE_BUCKET,
+	messagingSenderId: import.meta.MESSAGING_SENDER_ID,
+	appId: import.meta.APP_ID,
+};
+
+export const app = initializeApp(firebaseConfig);
