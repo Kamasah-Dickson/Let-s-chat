fc36f6af39f2a94cef741608f490176c4aac8fbd
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-18 02:17:53 +0000

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				sidebar_color: "#1d2026",
+				light_brown: "#2B2D34",
+				light_white: "#8888",
+				blue: "#0F80D7",
+				green: "#26FF8E",
+			},
+		},
 	},
 	plugins: [],
 };
