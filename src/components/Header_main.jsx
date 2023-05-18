573d744588dd975d6df7ee5c7d8cd5942ecf89f8
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-18 04:20:25 +0000

    update header

--- src/components/Header_main.jsx
+++ src/components/Header_main.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import testImage from "../assets/background.svg";
 import { MdSearch } from "react-icons/md";
 import { HiOutlineChatAlt2 } from "react-icons/hi";
 import { HiOutlineDotsVertical } from "react-icons/hi";
+import { AllContext } from "../context/appContext";
+
 function Header_main() {
+	const { setSearchFocus } = useContext(AllContext);
 	return (
 		<div className="bg-[#232733] border-b-[1px] border-[rgba(255,255,255,0.10)] px-5 flex transition-colors p-2 items-center gap-5 w-full">
 			<div className="rounded-full w-10 md:w-14 md:h-14 h-10  ">
@@ -23,7 +26,12 @@ function Header_main() {
 			<div>
 				<div className="flex gap-4 text-light_white">
 					<div className="w-[35px] h-[35px] flex items-center justify-center bg-[rgba(255,255,255,0.1)] rounded-full">
-						<MdSearch size={20} cursor={"pointer"} color="white" />
+						<MdSearch
+							onClick={() => setSearchFocus(true)}
+							size={20}
+							cursor={"pointer"}
+							color="white"
+						/>
 					</div>
 					<div className="w-[35px] h-[35px] flex items-center justify-center bg-[rgba(255,255,255,0.1)] rounded-full">
 						<HiOutlineChatAlt2 size={20} cursor={"pointer"} color="white" />
