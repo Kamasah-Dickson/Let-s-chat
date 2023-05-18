86e095cf62e9011d5f96cd01ff843bef4d273a92
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-18 03:48:20 +0000

    update sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
-import { MdSearch, MdMenu } from "react-icons/md";
+/* eslint-disable react/prop-types */
+import React, { useState, useContext } from "react";
+import { MdSearch, MdOutlineSettingsSuggest } from "react-icons/md";
 import Search from "./Search";
 import Sidebar_Singlechat from "./Sidebar_Singlechat";
 import Menu_options from "./Menu_options";
+import { Link } from "react-router-dom";
+import { AllContext } from "../context/appContext";
 
 function Sidebar() {
 	const [focus, setFocus] = useState(false);
-	const [options, setOptions] = useState(false);
+	const { options, setOptions } = useContext(AllContext);
 
 	return options ? (
 		<Menu_options setOptions={setOptions} />
 	) : (
-		<div className="p-3  border-[#191D24] border-r-2 h-screen overflow-y-auto bg-sidebar_color sticky top-20 left-0">
-			<MdMenu
+		<div className="p-3 border-[#191D24] border-r-2 h-screen overflow-y-auto bg-sidebar_color sticky top-20 left-0">
+			<MdOutlineSettingsSuggest
 				onClick={() => setOptions(true)}
 				color="white"
 				size={25}
@@ -21,29 +24,57 @@ function Sidebar() {
 			<div
 				onClick={() => setFocus(true)}
 				onBlur={() => setFocus(false)}
-				className="sticky mt-2 -top-2 overflow-hidden z-10 border-b-[1px] border-white bg-light_brown py-[7px] px-2 rounded-md flex items-center justify-between"
+				className="sticky mt-4 -top-2 overflow-hidden z-10 border-b-[1px] border-white bg-light_brown py-[7px] px-2 rounded-md flex items-center justify-between"
 			>
 				<Search focus={focus} placeholder="Search..." />
 				<MdSearch cursor={"pointer"} color="white" size={20} />
 			</div>
 			<div className="overflow-y-auto scrollbar flex flex-col gap-2 my-3">
-				<Sidebar_Singlechat />
-				<Sidebar_Singlechat />
-				<Sidebar_Singlechat />
-				<Sidebar_Singlechat />
-				<Sidebar_Singlechat />
-				<Sidebar_Singlechat />
-				<Sidebar_Singlechat />
-				<Sidebar_Singlechat />
-				<Sidebar_Singlechat />
-				<Sidebar_Singlechat />
-				<Sidebar_Singlechat />
-				<Sidebar_Singlechat />
-				<Sidebar_Singlechat />
-				<Sidebar_Singlechat />
-				<Sidebar_Singlechat />
-				<Sidebar_Singlechat />
-				<Sidebar_Singlechat />
+				<Link to="/">
+					<Sidebar_Singlechat />
+				</Link>
+				<Link to="/">
+					<Sidebar_Singlechat />
+				</Link>
+				<Link to="/">
+					<Sidebar_Singlechat />
+				</Link>
+				<Link to="/">
+					<Sidebar_Singlechat />
+				</Link>
+				<Link to="/">
+					<Sidebar_Singlechat />
+				</Link>
+				<Link to="/">
+					<Sidebar_Singlechat />
+				</Link>
+				<Link to="/">
+					<Sidebar_Singlechat />
+				</Link>
+				<Link to="/">
+					<Sidebar_Singlechat />
+				</Link>
+				<Link to="/">
+					<Sidebar_Singlechat />
+				</Link>
+				<Link to="/">
+					<Sidebar_Singlechat />
+				</Link>
+				<Link to="/">
+					<Sidebar_Singlechat />
+				</Link>
+				<Link to="/">
+					<Sidebar_Singlechat />
+				</Link>
+				<Link to="/">
+					<Sidebar_Singlechat />
+				</Link>
+				<Link to="/">
+					<Sidebar_Singlechat />
+				</Link>
+				<Link to="/">
+					<Sidebar_Singlechat />
+				</Link>
 			</div>
 		</div>
 	);
