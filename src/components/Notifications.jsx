2c5c58432d426affc5ff57c171666028e4e15ab1
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-18 03:46:23 +0000

    add notifications

---
+++ src/components/Notifications.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Notifications() {
+	return (
+		<div className="main-bg">
+			<div className="gradient h-full flex flex-col justify-center">
+				<div className="pb-44 pt-3 flex-[3] px-2 overflow-y-auto">
+					<p>dfsdfdfds</p>
+					<p>dfsdfdfds</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Notifications;
