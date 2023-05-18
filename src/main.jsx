2a84243bc62f4559ad4f5baaed09394a17f77737
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-18 03:47:31 +0000

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,30 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotificationPage from "./pages/NotificationPage.jsx";
+import ChatSettingsPage from "./pages/ChatSettingsPage.jsx";
+import AdvancedSettingsPage from "./pages/AdvancedSettingsPage.jsx";
+import LanguagePage from "./pages/LanguagePage.jsx";
+import AskQuestionsPage from "./pages/AskQuestionsPage.jsx";
+import AppContext from "./context/appContext.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<AppContext>
+			<Router>
+				<Routes>
+					<Route path="/" element={<App />}></Route>
+					<Route path="/notifications" element={<NotificationPage />}></Route>
+					<Route path="/chat-settings" element={<ChatSettingsPage />}></Route>
+					<Route
+						path="/advanced-settings"
+						element={<AdvancedSettingsPage />}
+					></Route>
+					<Route path="/language" element={<LanguagePage />}></Route>
+					<Route path="/ask-a-question" element={<AskQuestionsPage />}></Route>
+				</Routes>
+			</Router>
+		</AppContext>
 	</React.StrictMode>
 );
