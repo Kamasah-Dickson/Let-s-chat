ccc948f5cab70b7b53ee77ebe89f472dff39a13b
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-19 02:47:59 +0000

    add signup and login pages

---
+++ src/components/Signup.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+// import testing from "../assets/background.svg";
+
+function Signup() {
+	return (
+		<form>
+			<p>hello</p>
+		</form>
+	);
+}
+
+export default Signup;
