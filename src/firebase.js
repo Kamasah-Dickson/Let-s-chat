b9f5bca181a08220669f6a55b8856a11bf21acfd
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-20 04:31:17 +0000

    fix:fix Vite . env imports

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: import.meta.API_KEY,
-	authDomain: import.meta.AUTH_DOMAIN,
-	projectId: import.meta.PROJECTID,
-	storageBucket: import.meta.STORAGE_BUCKET,
-	messagingSenderId: import.meta.MESSAGING_SENDER_ID,
-	appId: import.meta.APP_ID,
+	apiKey: import.meta.env.VITE_API_KEY,
+	authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_PROJECTID,
+	storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
+
 
 export const auth = getAuth();
