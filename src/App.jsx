f955fd942dcdf2c3f4c3c25ecce5f284931d66f9
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-20 04:29:13 +0000

    update:redirect if user is not logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
+import useAuth from "./Hooks/auth";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+	const { user } = useAuth();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!user) {
+			navigate("/login");
+		}
+	}, [user]);
+
 	return (
 		<div className="flex">
 			<div className="flex-[8] sm:flex-[6] md:flex-[5] lg:flex-[4] xl:flex-[2.5]">
