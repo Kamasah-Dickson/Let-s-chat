432176c5aaf83d1ef6a4ef0c4a519fad5d49e519
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-21 00:51:13 +0000

    add: update profile page

---
+++ src/pages/UpdateProfilePage.jsx
@@ -0,0 +1,26 @@
+import React, { useContext, useLayoutEffect } from "react";
+import Sidebar from "../components/Sidebar";
+import UpdateProfile from "../components/UpdateProfile";
+import { AllContext } from "../context/appContext";
+
+function UpdateProfilePage() {
+	const { setsettings } = useContext(AllContext);
+	useLayoutEffect(() => {
+		setsettings(true);
+	}, []);
+
+	return (
+		<div>
+			<div className="flex">
+				<div className="flex-[12] sm:flex-[7] md:flex-[5] lg:flex-[4] xl:flex-[2.5]">
+					<Sidebar />
+				</div>
+				<div className="flex-[8] ">
+					<UpdateProfile />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default UpdateProfilePage;
