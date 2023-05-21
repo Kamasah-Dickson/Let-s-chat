4ec39eeb81988dafd41db176ca19962f977bfed4
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-21 01:02:41 +0000

    update:language page

--- src/pages/LanguagePage.jsx
+++ src/pages/LanguagePage.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useContext, useLayoutEffect } from "react";
 import Sidebar from "../components/Sidebar";
 import Language from "../components/Language";
+import { AllContext } from "../context/appContext";
 
 function LanguagePage() {
+	const { setsettings } = useContext(AllContext);
+	useLayoutEffect(() => {
+		setsettings(true);
+	}, []);
+
 	return (
 		<div className="flex max justify-center">
 			<div className="flex-[12] sm:flex-[7] md:flex-[5] lg:flex-[4] xl:flex-[2.5]">
