4f04ca90ba0d8ce47089aa93d0ac8808f41007bd
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-21 00:52:41 +0000

    update: modal overlay

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,16 +1,27 @@
 /* eslint-disable react/prop-types */
-import React, { memo } from "react";
+import React, { memo, useRef } from "react";
 import { TbAlertTriangleFilled } from "react-icons/tb";
 
 function Modal({ setAlert, notify }) {
+	const overlayRef = useRef(null);
+
+	function handleClick(e) {
+		if (e.target === overlayRef.current) {
+			setAlert(false);
+		}
+	}
 	return (
-		<div className=" z-50 absolute bg-[#000000a6] top-0 left-0 h-screen flex items-center justify-center w-full">
-			<div className="bg-[#2e323e] p-7 rounded-lg text-white">
-				<div className="flex flex-col items-center gap-5">
-					<TbAlertTriangleFilled size={50} color="yellow" />
+		<div
+			ref={overlayRef}
+			onClick={(e) => handleClick(e)}
+			className="z-50 cursor-pointer absolute bg-[#000000c2] top-0 left-0 h-screen flex items-center justify-center w-full"
+		>
+			<div className="bg-[#2e323e] cursor-default py-10 px-7  rounded-md text-white">
+				<div className="flex flex-col items-center gap-3">
+					<TbAlertTriangleFilled size={60} color="yellow" />
 					<p className="text-lg">{notify || "Try again An error occured"}</p>
 				</div>
-				<div className="mt-5 gap-7 flex items-center justify-center">
+				<div className="mt-7 gap-7 flex items-center justify-center">
 					<button
 						onClick={() => setAlert(false)}
 						className=" active:scale-[1.03] text-lg rounded cursor-pointer bg-[crimson] p-1 px-5"
