78fa5cb0844a34d8451b6923259b55844e7272d3
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-21 00:56:56 +0000

    update: settings

--- src/pages/AdvancedSettingsPage.jsx
+++ src/pages/AdvancedSettingsPage.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useContext, useLayoutEffect } from "react";
 import Sidebar from "../components/Sidebar";
 import AdvancedSettings from "../components/AdvancedSettings";
+import { AllContext } from "../context/appContext";
 
 function AdvancedSettingsPage() {
+	const { setsettings } = useContext(AllContext);
+	useLayoutEffect(() => {
+		setsettings(true);
+	}, []);
+
 	return (
 		<div className="flex max justify-center">
 			<div className="flex-[12] sm:flex-[7] md:flex-[5] lg:flex-[4] xl:flex-[2.5]">
