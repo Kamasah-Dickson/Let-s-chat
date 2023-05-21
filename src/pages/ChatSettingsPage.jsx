80ceec0d8321e0a8f5c034b635b617c364a3566d
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-21 01:02:17 +0000

    update: chat settings page

--- src/pages/ChatSettingsPage.jsx
+++ src/pages/ChatSettingsPage.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useContext, useLayoutEffect } from "react";
 import Sidebar from "../components/Sidebar";
 import Chat_settings from "../components/Chat_settings";
+import { AllContext } from "../context/appContext";
 
 function ChatSettingsPage() {
+	const { setsettings } = useContext(AllContext);
+	useLayoutEffect(() => {
+		setsettings(true);
+	}, []);
+
 	return (
 		<div className="flex max justify-center">
 			<div className="flex-[12] sm:flex-[7] md:flex-[5] lg:flex-[4] xl:flex-[2.5]">
