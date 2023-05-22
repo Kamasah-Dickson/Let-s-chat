46abcf0f409a77894d63489057a3d18bc098e731
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-22 03:48:28 +0000

    update: userProfile added to global context

--- src/context/appContext.jsx
+++ src/context/appContext.jsx
@@ -7,6 +7,11 @@ function AppContext({ children }) {
 	const [settings, setsettings] = useState(false);
 	const [searchFocus, setSearchFocus] = useState(false);
 	const [options, setOptions] = useState(true);
+	const [userProfile, setUserProfile] = useState({
+		photoUrl: "",
+		userName: "",
+		email: "",
+	});
 
 	return (
 		<AllContext.Provider
@@ -17,6 +22,8 @@ function AppContext({ children }) {
 				searchFocus,
 				settings,
 				setsettings,
+				userProfile,
+				setUserProfile,
 			}}
 		>
 			{children}
