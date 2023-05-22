4aae7da11e3250b16d948c95f6cd77901146bb54
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-22 13:41:11 +0000

    Fix: sidebar on smaller devices

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
 import { useNavigate } from "react-router-dom";
+import useMediaQuery from "./Hooks/useMediaQuery";
+import { AllContext } from "./context/appContext";
 
 function App() {
 	const navigate = useNavigate();
+	const { matches } = useMediaQuery("max-width:768px");
+	const { showTargetMessage } = useContext(AllContext);
 
 	useEffect(() => {
 		function deterMineUserLoggedIn() {
@@ -23,12 +27,27 @@ function App() {
 
 	return (
 		<div className="flex max">
-			<div className="flex-[8] sm:flex-[6] md:flex-[5] lg:flex-[4] xl:flex-[2.5]">
-				<Sidebar />
-			</div>
-			<div className="flex-[8] ">
-				<Main />
-			</div>
+			{matches && !showTargetMessage ? (
+				<div className="flex-[8] sm:flex-[6] md:flex-[5] lg:flex-[4] xl:flex-[2.5]">
+					<Sidebar />
+				</div>
+			) : (
+				!matches && (
+					<>
+						<div className="flex-[8] sm:flex-[6] md:flex-[5] lg:flex-[4] xl:flex-[2.5]">
+							<Sidebar />
+						</div>
+						<div className="flex-[8] ">
+							<Main />
+						</div>
+					</>
+				)
+			)}
+			{showTargetMessage && matches && (
+				<div className="flex-[8] ">
+					<Main />
+				</div>
+			)}
 		</div>
 	);
 }
