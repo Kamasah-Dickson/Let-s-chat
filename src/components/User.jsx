4963c97511cac4eeb4dc6978a390a468dad57ccb
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-22 03:52:23 +0000

    update: userProfile with corresponding credentials

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import testImage from "../assets/background.svg";
 import edit from "../assets/edit.svg";
 import { Link } from "react-router-dom";
+import { AllContext } from "../context/appContext";
 function User() {
+	const { userProfile } = useContext(AllContext);
+
 	return (
 		<div className="flex transition-colors p-2 items-center gap-5 w-full">
 			<div className="cursor-pointer rounded-full w-10 md:w-16 md:h-16 h-10 relative ">
 				<img
 					className="h-full w-full object-cover rounded-full"
-					src={testImage}
-					alt=""
+					src={userProfile.photoUrl || testImage}
+					alt={userProfile.userName}
 				/>
 			</div>
 			<div className="flex-1 ">
-				<h3 className="name">Kamasah Dickson</h3>
-				<span className="message text-green">@Kamasahdickson</span>
+				<h3 className="name">{userProfile.userName}</h3>
+				<span className="message text-green">
+					@{userProfile?.userName.toLowerCase()}
+				</span>
 			</div>
 			<div>
 				<Link
