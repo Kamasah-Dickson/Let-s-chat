81c73585df454856444d5c2a1964a1a6d5a19e53
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-22 13:35:56 +0000

    add: useMediaQuery to determine device screens

---
+++ src/Hooks/useMediaQuery.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+function useMediaQuery(query) {
+	const [matches, setMatches] = useState(false);
+
+	useEffect(() => {
+		const mediaQuery = window.matchMedia(`(${query})`);
+		if (mediaQuery.matches !== matches) {
+			setMatches(mediaQuery.matches);
+		}
+		const listener = () => setMatches(mediaQuery.matches);
+		window.addEventListener("resize", listener);
+
+		return () => window.removeEventListener("resize", listener);
+	}, [query, matches]);
+
+	return { matches };
+}
+
+export default useMediaQuery;
