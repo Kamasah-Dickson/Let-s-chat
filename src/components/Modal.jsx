0a42be6d9dea883c0ba7aafa8fece22a45eae8ab
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-22 13:40:16 +0000

    fix: modals on smaller devices

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -14,12 +14,14 @@ function Modal({ setAlert, notify }) {
 		<div
 			ref={overlayRef}
 			onClick={(e) => handleClick(e)}
-			className="z-50 cursor-pointer absolute bg-[#000000c2] top-0 left-0 h-screen flex items-center justify-center w-full"
+			className="z-50 cursor-pointer absolute bg-[#000000c2] top-0 left-0 h-screen flex items-center w-full"
 		>
-			<div className="bg-[#2e323e] cursor-default py-10 px-7  rounded-md text-white">
+			<div className="bg-[#2e323e] cursor-default my-max p-6 sm:p-10 rounded-md text-white">
 				<div className="flex flex-col items-center gap-3">
 					<TbAlertTriangleFilled size={60} color="yellow" />
-					<p className="text-lg">{notify || "Try again An error occured"}</p>
+					<p className="text-lg text-center">
+						{notify || "Try again An error occured"}
+					</p>
 				</div>
 				<div className="mt-7 gap-7 flex items-center justify-center">
 					<button
