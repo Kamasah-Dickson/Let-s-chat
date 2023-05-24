85d7c1d860b92701404203b8610cd472a7a63fc8
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-24 02:37:50 +0000

    FIX: responsiveness on smaller devices

--- src/components/AdvancedSettings.jsx
+++ src/components/AdvancedSettings.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AllContext } from "../context/appContext";
+import { HiOutlineArrowSmLeft } from "react-icons/hi";
 
 function AdvancedSettings() {
+	const { setToggleSettingsCategory } = useContext(AllContext);
 	return (
 		<div className="main-bg">
 			<div className="gradient h-full flex flex-col justify-center">
+				<HiOutlineArrowSmLeft
+					onClick={() => setToggleSettingsCategory(false)}
+					className="md:hidden m-3 flex"
+					color="white"
+					size={25}
+					cursor={"pointer"}
+				/>
 				<div className="pb-44 pt-3 flex-[3] px-2 overflow-y-auto">
 					<p>dfsdfdfds</p>
 					<p>dfsdfdfds</p>
