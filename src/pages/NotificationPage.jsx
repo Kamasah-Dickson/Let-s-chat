db0f04288f15cde734582cb802a7422fdc1fcf9d
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-24 02:32:35 +0000

    FIX: responsiveness  on smaller devices

--- src/pages/NotificationPage.jsx
+++ src/pages/NotificationPage.jsx
@@ -2,23 +2,38 @@ import React, { useContext, useLayoutEffect } from "react";
 import Sidebar from "../components/Sidebar";
 import Notifications from "../components/Notifications";
 import { AllContext } from "../context/appContext";
+import useMediaQuery from "../Hooks/useMediaQuery";
 
 function NotificationPage() {
-	const { setsettings } = useContext(AllContext);
+	const { setsettings, toggleSettingsCategory } = useContext(AllContext);
+	const { matches } = useMediaQuery("max-width:768px");
 	useLayoutEffect(() => {
 		setsettings(true);
 	}, []);
 
 	return (
-		<div>
-			<div className="flex">
-				<div className="flex-[12] sm:flex-[7] md:flex-[5] lg:flex-[4] xl:flex-[2.5]">
+		<div className="flex max">
+			{matches && !toggleSettingsCategory ? (
+				<div className="flex-[8] sm:flex-[6] md:flex-[5] lg:flex-[4] xl:flex-[2.5]">
 					<Sidebar />
 				</div>
+			) : (
+				!matches && (
+					<>
+						<div className="flex-[8] sm:flex-[6] md:flex-[5] lg:flex-[4] xl:flex-[2.5]">
+							<Sidebar />
+						</div>
+						<div className="flex-[8] ">
+							<Notifications />
+						</div>
+					</>
+				)
+			)}
+			{toggleSettingsCategory && matches && (
 				<div className="flex-[8] ">
 					<Notifications />
 				</div>
-			</div>
+			)}
 		</div>
 	);
 }
