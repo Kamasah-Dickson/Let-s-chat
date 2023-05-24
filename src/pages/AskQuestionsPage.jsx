add3c9d70f4830a1003fdb545de965a94e5f3712
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-24 02:33:57 +0000

    FIX: responsiveness  on smaller devices

--- src/pages/AskQuestionsPage.jsx
+++ src/pages/AskQuestionsPage.jsx
@@ -2,21 +2,39 @@ import React, { useContext, useLayoutEffect } from "react";
 import Sidebar from "../components/Sidebar";
 import AskQuestions from "../components/AskQuestions";
 import { AllContext } from "../context/appContext";
+import useMediaQuery from "../Hooks/useMediaQuery";
 
 function AskQuestionsPage() {
-	const { setsettings } = useContext(AllContext);
+	const { setsettings, toggleSettingsCategory } = useContext(AllContext);
+	const { matches } = useMediaQuery("max-width:768px");
+
 	useLayoutEffect(() => {
 		setsettings(true);
 	}, []);
 
 	return (
-		<div className="flex max justify-center">
-			<div className="flex-[12] sm:flex-[7] md:flex-[5] lg:flex-[4] xl:flex-[2.5]">
-				<Sidebar />
-			</div>
-			<div className="flex-[8] ">
-				<AskQuestions />
-			</div>
+		<div className="flex max">
+			{matches && !toggleSettingsCategory ? (
+				<div className="flex-[8] sm:flex-[6] md:flex-[5] lg:flex-[4] xl:flex-[2.5]">
+					<Sidebar />
+				</div>
+			) : (
+				!matches && (
+					<>
+						<div className="flex-[8] sm:flex-[6] md:flex-[5] lg:flex-[4] xl:flex-[2.5]">
+							<Sidebar />
+						</div>
+						<div className="flex-[8] ">
+							<AskQuestions />
+						</div>
+					</>
+				)
+			)}
+			{toggleSettingsCategory && matches && (
+				<div className="flex-[8] ">
+					<AskQuestions />
+				</div>
+			)}
 		</div>
 	);
 }
