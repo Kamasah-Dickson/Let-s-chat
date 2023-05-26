02e1234e6342459497b4a34248bd7870884bcee0
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-26 03:18:16 +0000

    add realtime database

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
 	apiKey: import.meta.env.VITE_API_KEY,
 	authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+	databaseURL: import.meta.env.VITE_DATABASE_URL,
 	projectId: import.meta.env.VITE_PROJECTID,
 	storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
 	messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
@@ -15,4 +16,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
-export const firestore = getFirestore(app);
+export const db = getDatabase(app);
