2dc4bc68fe33f338a9254d1366b3b10e8420d5db
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-27 04:38:44 +0000

    udpate main

--- src/main.jsx
+++ src/main.jsx
@@ -13,23 +13,29 @@ import AppContext from "./context/appContext.jsx";
 import Signup from "./components/Signup.jsx";
 import Login from "./components/Login.jsx";
 import UpdateProfilePage from "./pages/UpdateProfilePage.jsx";
+import ChatContextProvider from "./context/chatContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<AppContext>
-			<Router>
-				<Routes>
-					<Route path="/" element={<App />} />
-					<Route path="/signup" element={<Signup />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/profileupdate" element={<UpdateProfilePage />} />
-					<Route path="/notifications" element={<NotificationPage />} />
-					<Route path="/chat-settings" element={<ChatSettingsPage />} />
-					<Route path="/advanced-settings" element={<AdvancedSettingsPage />} />
-					<Route path="/language" element={<LanguagePage />} />
-					<Route path="/ask-a-question" element={<AskQuestionsPage />} />
-				</Routes>
-			</Router>
+			<ChatContextProvider>
+				<Router>
+					<Routes>
+						<Route path="/" element={<App />} />
+						<Route path="/signup" element={<Signup />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/profileupdate" element={<UpdateProfilePage />} />
+						<Route path="/notifications" element={<NotificationPage />} />
+						<Route path="/chat-settings" element={<ChatSettingsPage />} />
+						<Route
+							path="/advanced-settings"
+							element={<AdvancedSettingsPage />}
+						/>
+						<Route path="/language" element={<LanguagePage />} />
+						<Route path="/ask-a-question" element={<AskQuestionsPage />} />
+					</Routes>
+				</Router>
+			</ChatContextProvider>
 		</AppContext>
 	</React.StrictMode>
 );
