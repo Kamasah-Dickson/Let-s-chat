04e43dac5a3c27ece5bc77441f1b21d26a087406
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-06-01 14:46:05 +0000

    FIX:unable to clear input when a message is sent

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -15,6 +15,7 @@ import {
 	serverTimestamp,
 	get,
 	update,
+	push,
 	set,
 	child,
 } from "firebase/database";
@@ -24,39 +25,20 @@ import { uploadBytesResumable, getDownloadURL } from "firebase/storage";
 function Main() {
 	const { setOptions, combinedID } = useContext(AllContext);
 	const textareaRef = useRef(null);
-	const [messages, setMessages] = useState([]);
-	const { data, setNewMessage, selectedUserID } = useContext(ChatContext);
+	const { data, setNewMessage, selectedUserID, messages, setMessages } =
+		useContext(ChatContext);
 
 	const [text, setText] = useState("");
 	const [img, setImg] = useState("");
 
-	const handleInputChange = () => {
+	const handleInputChange = (e) => {
 		const textarea = textareaRef.current;
 		textarea.style.height = "auto";
 		textarea.style.height = `${textarea.scrollHeight}px`;
+		setText(e.target.value);
 	};
 
 	const currentUserId = auth?.currentUser?.uid;
-
-	//do it at the sidebar
-
-	// useEffect(() => {
-	// 	const chatRef = ref(db, "newMessages");
-	// 	const chatsListener = onValue(chatRef, (snapshot) => {
-	// 		const data = snapshot.val();
-	// 		// Convert the object of chats into an array
-	// 		const chatArray = Object.keys(data || {}).map((key) => ({
-	// 			chatId: key,
-	// 			...data[key],
-	// 		}));
-	// 		setNewMessage(chatArray);
-	// 	});
-
-	// 	return () => {
-	// 		// Detach the listener
-	// 		off(chatRef, chatsListener);
-	// 	};
-	// }, []);
 
 	useEffect(() => {
 		const messagesRef = ref(db, "chats/" + combinedID + "/messages");
@@ -69,14 +51,13 @@ function Main() {
 			}));
 
 			setMessages(messageArray);
-			setText("");
 		});
 
 		return () => {
 			// Detach the listener
 			off(messagesRef, messagesListener);
 		};
-	}, [combinedID, text]);
+	}, [combinedID]);
 
 	const handleSend = async () => {
 		if (img) {
@@ -92,12 +73,14 @@ function Main() {
 						img: downloadURL,
 						date: serverTimestamp(),
 					};
-					const userRef = ref(db, `chats/${data.chatId}`);
-					await update(userRef, {
-						[`messages/${uuid()}`]: message,
-					});
+
+					const userRef = ref(db, `chats/${data.chatId}/messages`);
+					const newMessageRef = push(userRef);
+					await set(newMessageRef, message);
+					setText("");
 				});
 			});
+			setText("");
 		} else {
 			const message = {
 				id: uuid(),
@@ -106,10 +89,10 @@ function Main() {
 				date: serverTimestamp(),
 			};
 
-			const userRef = ref(db, `chats/${data.chatId}`);
-			await update(userRef, {
-				[`messages/${uuid()}`]: message,
-			});
+			const userRef = ref(db, `chats/${data.chatId}/messages`);
+			const newMessageRef = push(userRef);
+			await set(newMessageRef, message);
+			setText("");
 
 			const chatRef = ref(db, "newMessages");
 			const currentUserChatRef = child(chatRef, selectedUserID);
@@ -132,8 +115,7 @@ function Main() {
 			onValue(chatRef, (snapshot) => {
 				const data = snapshot.val();
 				if (data) {
-					const nestedKey = Object.entries(data); // Get the key of the nested object
-					// const newMessage = data[nestedKey]; // Access the message value
+					const nestedKey = Object.entries(data).reverse(); // Get the key of the nested object
 					setNewMessage(nestedKey);
 				}
 			});
@@ -146,15 +128,18 @@ function Main() {
 			<div className="gradient my-height overflow-y-scroll flex flex-col justify-center">
 				<div
 					onClick={() => setOptions(true)}
-					className="flex pt-5 flex-col gap-5   flex-[3] px-5 overflow-y-auto"
+					className="flex pt-5 flex-col gap-3 flex-[3] px-5 overflow-y-auto"
 				>
 					{messages.map((message) => (
 						<Message key={message.id} message={message} />
 					))}
+
 					<label
 						htmlFor="message"
-						className="rounded-md h-[50px] mt-auto items-center shadow-sm shadow-[#0000004f] text-white bg-[#2E323C] md:my-full gap-5 w-full flex sticky
-						bottom-5 md:bottom-10 p-3"
+						className={`${
+							messages.length > 6 ? "mt-[5rem] " : "mt-auto "
+						}rounded-md h-[50px] items-center shadow-sm shadow-[#0000004f] text-white bg-[#2E323C] md:my-full gap-5 w-full flex sticky
+						bottom-5 md:bottom-10 p-3`}
 					>
 						<input
 							type="file"
@@ -174,7 +159,7 @@ function Main() {
 						<textarea
 							rows="1"
 							ref={textareaRef}
-							onChange={(e) => (handleInputChange(), setText(e.target.value))}
+							onChange={(e) => handleInputChange(e)}
 							autoCorrect="true"
 							autoComplete="true"
 							id="message"
