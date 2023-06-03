844b577a66d4cef2fb8dea6b681b063c3fe23c21
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-06-03 04:49:54 +0000

    add:coming soon page

---
+++ src/components/ComingSoon.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ComingSoon() {
+	return (
+		<div className="flex items-center h-full justify-center flex-col">
+			<span className="text-7xl mb-5">💬</span>
+			<h1 className="text-white text-3xl md:text-4xl">Feature Coming Soon🔥</h1>
+			<p className="text-lg text-[#d4d4f7d2] mt-1">
+				{`Enjoy the time at let's chat`} 💭
+			</p>
+		</div>
+	);
+}
+
+export default ComingSoon;
