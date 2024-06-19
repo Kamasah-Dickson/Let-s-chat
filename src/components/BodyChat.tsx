1fd02904178f4536c43b245cd6e210f25665e59b
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-06-19 16:40:25 +0000

    feat:focus chatInput when contact is changed

--- src/components/BodyChat.tsx
+++ src/components/BodyChat.tsx
@@ -91,7 +91,7 @@ const BodyChat = ({ messages }: IBodyChat) => {
 			onClick={() =>
 				dispatch({ type: "setOptions", payload: { options: false } })
 			}
-			className="flex overflow-y-auto flex-col gap-7 py-2 px-5 h-screen"
+			className="flex flex-col gap-7 py-2 px-5"
 		>
 			{messages.length >= 1 ? (
 				messages.map((message: IMessage, index) => (
