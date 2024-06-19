c2d1c4ebd5fbfc09264836177c24303da0e11884
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-06-19 17:18:34 +0000

    fix sidebar search

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -7,7 +7,6 @@ import { IChat, setShowSidebar } from "../Store/features/chatSlice";
 import ChatInput from "./ChatInput";
 import BodyChat from "./BodyChat";
 import { MdMenu } from "react-icons/md";
-import { useEffect } from "react";
 
 export type ImgType = {
 	type: Blob | MediaSource;
@@ -29,10 +28,6 @@ function Main() {
 	) as IChat;
 
 	const dispatch = useDispatch<AppDispatch>();
-
-	useEffect(() => {
-		document.body.style.overflow = "hidden";
-	}, []);
 
 	return (
 		<div className="main-bg">
