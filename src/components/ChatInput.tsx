a70766d45b53b00aa89aedda6d098675835a4c2d
Kamasah Dickson <kamasahdickson19@gmail.com>
2024-06-22 04:55:34 +0100

    fix overflow

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -235,7 +235,7 @@ const ChatInput = ({ partneredChat }: IChatInput) => {
 	}, []);
 
 	return (
-		<div className={`sticky bottom-7 mt-5 mx-5`}>
+		<div className={`sticky bottom-7 mx-5`}>
 			{img && (
 				<div className="px-5">
 					<div
